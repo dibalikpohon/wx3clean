c4803a9247e97bb8f217ccdbfe2f09313ffd92c0
Karunia Sentosa <dibalikpohon@gmail.com>
2023-07-31 16:33:45 +0700

    create data not exists error

---
+++ domain/repo/errors/data_not_exists.go
@@ -0,0 +1,30 @@
+package repo
+
+import "fmt"
+
+type What struct {
+	Field string `json:"field"`
+	Value string `json:"value"`
+}
+
+type DataNotExistsError struct {
+	Message string `json:"message"`
+	What    What   `json:"what"`
+}
+
+func NewDataNotExistsError(field, value string) *DataNotExistsError {
+	what := What{
+		Field: field,
+		Value: value,
+	}
+
+	return &DataNotExistsError{
+		Message: "Data tidak ditemukan",
+		What:    what,
+	}
+}
+
+func (e *DataNotExistsError) Error() string {
+	return fmt.Sprintf("%v. (%v: %v)",
+		e.Message, e.What.Field, e.What.Value)
+}
