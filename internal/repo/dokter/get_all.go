66c56e83af251e02a5902e07fa940c95e3c8f393
Karunia Sentosa <dibalikpohon@gmail.com>
2023-07-31 16:32:45 +0700

    complete get all dokter implementation

--- internal/repo/dokter/get_all.go
+++ internal/repo/dokter/get_all.go
@@ -1,8 +1,26 @@
 package dokter
 
-import "wx3clean/domain/entities"
+import (
+	"wx3clean/domain/entities"
+	"wx3clean/internal/db"
+)
 
 // GetAll implements repo.BasicAction.
-func (r *DokterRepo) GetAll() ([]entities.Dokter, error) {
-	panic("unimplemented")
+func (r *DokterRepo) GetAll() (arr []entities.Dokter, err error) {
+
+	db := db.MysqlGetInstance()
+	sql := `SELECT id, nama, spesialisasi, tarif_konsultasi FROM dokters`
+
+	rows, err := db.Query(sql)
+	if err != nil {
+		panic(err)
+	}
+	for rows.Next() {
+		var e entities.Dokter
+		rows.Scan(&e.Id, &e.Nama, &e.Spesialisasi, &e.TarifKonsultasi)
+
+		arr = append(arr, e)
+	}
+
+	return
 }
