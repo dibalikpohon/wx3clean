6f3778713fb002e4c9f8e4af928e42bfa62d607a
Karunia Sentosa <dibalikpohon@gmail.com>
2023-07-31 16:33:03 +0700

    complete delete dokter repo implementation

--- internal/repo/dokter/delete.go
+++ internal/repo/dokter/delete.go
@@ -1,6 +1,26 @@
 package dokter
 
+import (
+	"errors"
+	repoerr "wx3clean/domain/repo/errors"
+	"wx3clean/internal/db"
+)
+
 // Delete implements repo.BasicAction.
 func (r *DokterRepo) Delete(id string) error {
-	panic("unimplemented")
+
+	_, err := r.GetById(id)
+	var dataNotExists *repoerr.DataNotExistsError
+	if errors.As(err, &dataNotExists) {
+		return err
+	}
+
+	db := db.MysqlGetInstance()
+	query_s := `DELETE FROM dokters WHERE id = ?`
+	_, err = db.Exec(query_s, id)
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
 }
