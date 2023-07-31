8902156587705213389d2abc50c229516fe716cd
Karunia Sentosa <dibalikpohon@gmail.com>
2023-07-31 16:33:13 +0700

    implement update dokter repo implementation

--- internal/repo/dokter/update.go
+++ internal/repo/dokter/update.go
@@ -1,8 +1,34 @@
 package dokter
 
-import "wx3clean/domain/entities"
+import (
+	"errors"
+	"wx3clean/domain/entities"
+	repoerr "wx3clean/domain/repo/errors"
+	"wx3clean/internal/db"
+)
 
 // Update implements repo.BasicAction.
-func (r *DokterRepo) Update(id string, dao entities.Dokter) error {
-	panic("unimplemented")
+func (r *DokterRepo) Update(id string, dao entities.Dokter) (err error) {
+
+	_, err = r.GetById(id)
+	var dataNotExists *repoerr.DataNotExistsError
+	if errors.As(err, &dataNotExists) {
+		return err
+	}
+
+	db := db.MysqlGetInstance()
+	query_s := `
+	  UPDATE dokters 
+	  SET 
+	    nama=?, 
+	    spesialisasi = ?, 
+	    tarif_konsultasi = ? 
+	  WHERE id = ?
+	`
+	_, err = db.Exec(query_s, dao.Nama, dao.Spesialisasi, dao.TarifKonsultasi, id)
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
 }
