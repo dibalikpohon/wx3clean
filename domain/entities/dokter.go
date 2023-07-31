8ad3cefbd5f5de9f8ce91676f7a57ad0c4a3db92
Karunia Sentosa <dibalikpohon@gmail.com>
2023-07-31 20:36:53 +0700

    remove json tag in entities because we don't need it

--- domain/entities/dokter.go
+++ domain/entities/dokter.go
@@ -1,8 +1,8 @@
 package entities
 
 type Dokter struct {
-	Id              string  `json:"id"`
-	Nama            string  `json:"nama"`
-	Spesialisasi    string  `json:"spesialisasi"`
-	TarifKonsultasi float32 `json:"tarif_konsultasi"`
+	Id              string
+	Nama            string
+	Spesialisasi    string
+	TarifKonsultasi float32
 }
