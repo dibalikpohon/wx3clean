0753184e6498236f33928ebe984d36ab9cbfb0d7
Karunia Sentosa <dibalikpohon@gmail.com>
2023-07-31 09:25:10 +0700

    add dokter handlre

---
+++ internal/handler/dokter/handler.go
@@ -0,0 +1,42 @@
+package dokter
+
+import (
+	"net/http"
+	handler "wx3clean/domain/handler"
+	usecases "wx3clean/domain/usecases/dokter"
+)
+
+type dokterHandler struct {
+	addDokter     usecases.AddDokterUsecase
+	deleteDokter  usecases.DeleteDokterUsecase
+	getAllDokter  usecases.GetAllDokterUsecase
+	getDokterById usecases.GetDokterByIdUsecase
+	updateDokter  usecases.UpdateDokterUsecase
+}
+
+// GetHandlers implements handler.HandlerInterface.
+func (h *dokterHandler) GetHandlers() (hs []handler.HandlerStruct) {
+	hs = append(hs,
+		handler.HandlerStruct{Method: http.MethodGet, Path: "/dokter", HandlerFunc: h.get}, // get All and get by id
+		handler.HandlerStruct{Method: http.MethodPost, Path: "/dokter", HandlerFunc: h.post},
+		handler.HandlerStruct{Method: http.MethodPut, Path: "/dokter", HandlerFunc: h.put},
+		handler.HandlerStruct{Method: http.MethodDelete, Path: "/dokter", HandlerFunc: h.delete},
+	)
+	return
+}
+
+func NewDokterHandler(
+	addDokterUsecase usecases.AddDokterUsecase,
+	deleteDokterUsecase usecases.DeleteDokterUsecase,
+	getAllDokterUsecase usecases.GetAllDokterUsecase,
+	getDokterByIdUsecase usecases.GetDokterByIdUsecase,
+	updateDokterUsecase usecases.UpdateDokterUsecase,
+) handler.HandlerInterface {
+	return &dokterHandler{
+		addDokter:     addDokterUsecase,
+		deleteDokter:  deleteDokterUsecase,
+		getAllDokter:  getAllDokterUsecase,
+		getDokterById: getDokterByIdUsecase,
+		updateDokter:  updateDokterUsecase,
+	}
+}
