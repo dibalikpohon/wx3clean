4e5a471c90b503ff5b70f7b4ff1ad6f345f96787
Karunia Sentosa <dibalikpohon@gmail.com>
2023-07-31 16:32:55 +0700

    complete insert dokter repo implementation

--- internal/repo/dokter/insert.go
+++ internal/repo/dokter/insert.go
@@ -1,8 +1,22 @@
 package dokter
 
-import "wx3clean/domain/entities"
+import (
+	"wx3clean/domain/entities"
+	"wx3clean/internal/db"
+)
 
 // Insert implements repo.BasicAction.
-func (r *DokterRepo) Insert(dao entities.Dokter) error {
-	panic("unimplemented")
+func (r *DokterRepo) Insert(dao entities.Dokter) (err error) {
+
+	db := db.MysqlGetInstance()
+	query_s := `
+	  INSERT INTO dokters (id, nama, spesialisasi, tarif_konsultasi)
+	  VALUES (?, ?, ?, ?)
+	`
+	_, err = db.Exec(query_s, dao.Id, dao.Nama, dao.Spesialisasi, dao.TarifKonsultasi)
+	if err != nil {
+		panic(err)
+	}
+
+	return
 }
