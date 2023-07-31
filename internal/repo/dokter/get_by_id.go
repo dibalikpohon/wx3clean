96b06b4e4000cfb31930119cf3c9a2e4dc468df1
Karunia Sentosa <dibalikpohon@gmail.com>
2023-07-31 16:32:37 +0700

    complete get dokter by id implementation

--- internal/repo/dokter/get_by_id.go
+++ internal/repo/dokter/get_by_id.go
@@ -1,8 +1,23 @@
 package dokter
 
-import "wx3clean/domain/entities"
+import (
+	"database/sql"
+	"errors"
+	"wx3clean/domain/entities"
+	repoerr "wx3clean/domain/repo/errors"
+	"wx3clean/internal/db"
+)
 
 // GetById implements repo.BasicAction.
-func (r *DokterRepo) GetById(id string) (entities.Dokter, error) {
-	panic("unimplemented")
+func (r *DokterRepo) GetById(id string) (v entities.Dokter, err error) {
+
+	db := db.MysqlGetInstance()
+	query_s := `SELECT id, nama, spesialisasi, tarif_konsultasi FROM dokters WHERE id = ?`
+
+	err = db.QueryRow(query_s, id).Scan(&v.Id, &v.Nama, &v.Spesialisasi, &v.TarifKonsultasi)
+	if errors.Is(err, sql.ErrNoRows) {
+		err = repoerr.NewDataNotExistsError("id", id)
+	}
+
+	return
 }
