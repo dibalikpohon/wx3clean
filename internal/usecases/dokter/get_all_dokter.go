7a81e3909f524881019ff2ac1bb4968fc33e5f6b
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-01 16:33:17 +0700

    add get all dokter usecase implementation

--- internal/usecases/dokter/get_all_dokter.go
+++ internal/usecases/dokter/get_all_dokter.go
@@ -3,7 +3,6 @@ package dokter
 import (
 	"wx3clean/domain/entities"
 	"wx3clean/domain/repo"
-	"wx3clean/domain/response"
 	"wx3clean/domain/usecases/dokter"
 )
 
@@ -12,8 +11,9 @@ type GetAllDokterUsecaseImpl struct {
 }
 
 // Execute implements dokter.GetAllDokterUsecase.
-func (*GetAllDokterUsecaseImpl) Execute() (response response.GetDokterArray, err error) {
-	panic("unimplemented")
+func (u *GetAllDokterUsecaseImpl) Execute() (result []entities.Dokter, err error) {
+	result, err = u.getAllAction.GetAll()
+	return
 }
 
 func NewGetAllDokterUsecase(getAllAction repo.GetAllAction[entities.Dokter]) dokter.GetAllDokterUsecase {
