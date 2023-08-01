875c1e4692752014e611a01445620e6b7df8d938
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-01 16:34:48 +0700

    change insert function to return string and error

--- internal/repo/dokter/insert.go
+++ internal/repo/dokter/insert.go
@@ -3,17 +3,21 @@ package dokter
 import (
 	"wx3clean/domain/entities"
 	"wx3clean/internal/db"
+	"wx3clean/internal/helper"
 )
 
 // Insert implements repo.BasicAction.
-func (r *DokterRepo) Insert(dao entities.Dokter) (err error) {
+func (r *DokterRepo) Insert(dao entities.Dokter) (newId string, err error) {
 
 	db := db.MysqlGetInstance()
 	query_s := `
 	  INSERT INTO dokters (id, nama, spesialisasi, tarif_konsultasi)
 	  VALUES (?, ?, ?, ?)
 	`
-	_, err = db.Exec(query_s, dao.Id, dao.Nama, dao.Spesialisasi, dao.TarifKonsultasi)
+
+	newId = helper.RandomChar(15)
+
+	_, err = db.Exec(query_s, newId, dao.Nama, dao.Spesialisasi, dao.TarifKonsultasi)
 	if err != nil {
 		panic(err)
 	}
