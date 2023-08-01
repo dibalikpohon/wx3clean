fa58590e60e0ba111b1042b225e339ea11c1acbc
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-01 16:33:26 +0700

    add get dokter by id usecase implementation

--- internal/usecases/dokter/get_dokter_by_id.go
+++ internal/usecases/dokter/get_dokter_by_id.go
@@ -3,7 +3,6 @@ package dokter
 import (
 	"wx3clean/domain/entities"
 	"wx3clean/domain/repo"
-	"wx3clean/domain/response"
 	"wx3clean/domain/usecases/dokter"
 )
 
@@ -12,8 +11,9 @@ type GetDokterByIdUsecaseImpl struct {
 }
 
 // Execute implements dokter.GetDokterByIdUsecase.
-func (*GetDokterByIdUsecaseImpl) Execute(id string) (response response.GetDokter, err error) {
-	panic("unimplemented")
+func (u *GetDokterByIdUsecaseImpl) Execute(id string) (result entities.Dokter, err error) {
+	result, err = u.getByIdAction.GetById(id)
+	return
 }
 
 func NewGetDokterByIdUsecase(getByIdAction repo.GetByIdAction[entities.Dokter]) dokter.GetDokterByIdUsecase {
