2671e1ac45085450171bcf205276baee7c0edff4
Karunia Sentosa <dibalikpohon@gmail.com>
2023-08-01 16:33:02 +0700

    implement add dokter usecases

--- internal/usecases/dokter/add_dokter.go
+++ internal/usecases/dokter/add_dokter.go
@@ -3,7 +3,6 @@ package dokter
 import (
 	"wx3clean/domain/entities"
 	"wx3clean/domain/repo"
-	"wx3clean/domain/request"
 	"wx3clean/domain/usecases/dokter"
 )
 
@@ -12,8 +11,9 @@ type AddDokterUsecaseImpl struct {
 }
 
 // Execute implements dokter.AddDokterUsecase.
-func (*AddDokterUsecaseImpl) Execute(request request.AddDokter) (err error) {
-	panic("unimplemented")
+func (u *AddDokterUsecaseImpl) Execute(dokter entities.Dokter) (newId string, err error) {
+	newId, err = u.insertAction.Insert(dokter)
+	return
 }
 
 func NewAddDokterUsecase(insertAction repo.InsertAction[entities.Dokter]) dokter.AddDokterUsecase {
